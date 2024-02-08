8bf7d14b5ffdca458b2ad8875df44e45e75ee5f6
Alex Sobiek <alex@alexsobiek.com>
2024-02-08 16:49:59 -0600

    Created ProtocolConstants

---
+++ network/protocol/src/main/java/com/alexsobiek/terranova/network/protocol/ProtocolConstants.java
@@ -0,0 +1,7 @@
+package com.alexsobiek.terranova.network.protocol;
+
+public class ProtocolConstants {
+    public static int READ_TIME_OUT = 30;
+    public static int PROTOCOL_VERSION = 14;
+    public static int USERNAME_MAX_LENGTH = 16;
+}
